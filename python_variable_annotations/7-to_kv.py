9c4b2f959f21ca61b9e63a90f7beae65e6adfbaf
Iaskasan <nokiesan@yahoo.fr>
2024-08-13 10:57:03 +0200

    fixed a trailing whitespace

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -4,6 +4,6 @@ from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    '''concatene a string and the square of 
+    '''concatene a string and the square of
     an int/float and return a tuple with those elements'''
     return (k, v ** 2)
