944219ad00bc915be8c4634d1eddc5107f9d52f2
Iaskasan <nokiesan@yahoo.fr>
2024-08-13 15:35:37 +0200

    added 2nd task

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+'''multiple coroutines'''
+wait_random = __import__("0-basic_async_syntax").wait_random
+import asyncio
+
+
+async def wait_n(n, max_delay):
+  '''double awaiting'''
+    task = []
+    for _ in range(n):  
+        task.append(wait_random(max_delay))
+    result = await asyncio.gather(*task)
+    return result
