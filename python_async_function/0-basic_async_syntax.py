05acc0f8f2a17cd6d2b905cb4cc3c6b29bb95417
Iaskasan <nokiesan@yahoo.fr>
2024-08-13 15:20:35 +0200

    added first task

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+'''simple asynchrone method'''
+import asyncio
+from random import uniform
+
+
+async def wait_random(max_delay=10):
+    '''waits for a random delay between 0 and input
+    parameter then returns the delay as a float'''
+    random_delay = uniform(0, max_delay)
+    await asyncio.sleep(random_delay)
+    return random_delay
