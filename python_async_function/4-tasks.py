eba0c3e68078a333ed687c7f148c6e525b0091dd
Iaskasan <nokiesan@yahoo.fr>
2024-08-18 19:09:54 +0200

    added remaining tasks

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""funtion Tasks"""
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    '''double awaiting'''
+    result = []
+    for _ in range(n):
+        result.append(await task_wait_random(max_delay))
+    return sorted(result)
