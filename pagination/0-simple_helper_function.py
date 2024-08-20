35c0a8a3ec8368c72130564fa76675749a0ff751
Iaskasan <nokiesan@yahoo.fr>
2024-08-20 11:24:48 +0200

    added first task

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+
+def index_range(page: int, page_size: int) -> tuple:
+    '''
+    simple helper function that return the range
+    of an index
+    '''
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
