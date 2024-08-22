00d4ec5e4f2eb3b62c0420f9c1e12c609597b760
Iaskasan <nokiesan@yahoo.fr>
2024-08-22 10:20:50 +0200

    added topic finder task

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+'''return a list of string containing a specified topic'''
+
+
+def schools_by_topic(mongo_collection, topic):
+    '''return a list of string with a specified topic'''
+    schools = mongo_collection.find({"topics": topic})
+    return list(schools)
