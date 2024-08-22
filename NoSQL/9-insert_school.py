20b1ff5490340883697d83874124d563e1a60345
Iaskasan <nokiesan@yahoo.fr>
2024-08-22 09:57:40 +0200

    trying just .insert

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+'''insert new docs in the collection'''
+
+
+def insert_school(mongo_collection, **kwargs):
+    '''insert new docs in the collection
+    based on args passed to the function'''
+    mongo_collection.insert(**kwargs)
