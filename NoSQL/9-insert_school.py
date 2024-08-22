7c650181f8009e6001dc4fa7398c0dda17193c13
Iaskasan <nokiesan@yahoo.fr>
2024-08-22 10:10:23 +0200

    added condition

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -5,4 +5,6 @@
 def insert_school(mongo_collection, **kwargs):
     '''insert new docs in the collection
     based on args passed to the function'''
+    if mongo_collection is None:
+        return None
     return mongo_collection.insert(kwargs).inserted_id
