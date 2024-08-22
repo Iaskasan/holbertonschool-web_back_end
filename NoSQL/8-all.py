8a60c02f80d93efbe780f7bce177b51d6de5f5e7
Iaskasan <nokiesan@yahoo.fr>
2024-08-22 09:54:32 +0200

    added first orm use

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+'''Salut Lucas'''
+from pymongo import MongoClient
+
+
+def list_all(mongo_collection):
+    '''au revoir benoit'''
+    if mongo_collection is None:
+        return []
+    return list(mongo_collection.find())
