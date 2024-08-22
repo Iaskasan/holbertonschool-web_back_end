a2ba5c2d0bc13979afef319cb0add8bfa6bf0b81
Iaskasan <nokiesan@yahoo.fr>
2024-08-22 10:17:41 +0200

    added a task that update the toppics of a document

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+'''Update topics of a school document in the MongoDB collection'''
+
+
+def update_topics(mongo_collection, name, topics):
+    '''update topics of a specified document'''
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
